fa166f78d9c0c374efa79aa1c94b347f07a905d6
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2024-04-16 15:45:24 -0700

    Update iterator

--- factorial.sh
+++ factorial.sh
@@ -5,13 +5,13 @@ read -p "Enter an integer: " num
 if [[ num -ge 10 ]]; then
 	x=1
 	for i in {1..10}; do
-		x=$(($x * i))
+		((x *= i))
 	done
 	num=$(($num + x))
 else
 	x=1
 	for i in {1..5}; do
-		x=$(($x * i))
+		((x *= i))
 	done
 	num=$(($num - x))
 fi
--- loops.sh
+++ loops.sh
@@ -5,8 +5,7 @@ for i in {0..10}; do
 done
 
 x=0
-while [[ "${x}" -le 10 ]]
-do 
-  echo "${x}"
-  x=$(($x+1))
+while [[ "${x}" -le 10 ]]; do
+	echo "${x}"
+	((x++))
 done
